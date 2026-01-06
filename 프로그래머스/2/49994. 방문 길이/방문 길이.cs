96f168997d99b0cdc7553d5953c0a894b9cfc4d8
Yuchan Lee <flashedmite45@gmail.com>
2026-01-06 13:12:28 +0900

    [level 2] Title: 방문 길이, Time: 4.80 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.cs
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cs
@@ -1,59 +1,61 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
-public class Solution {    
-    public struct Point{
-        public int x;
-        public int y;
-    }
-    Dictionary<Point, HashSet<Point>> connect = new Dictionary<Point, HashSet<Point>>();
-    
-    public void Add(Point from, Point to){
-        if(!connect.ContainsKey(from)){
-            HashSet<Point> s = new HashSet<Point>();
-            connect.Add(from, s);
-        }
-        if(!connect.ContainsKey(to)){
-            HashSet<Point> s = new HashSet<Point>();
-            connect.Add(to, s);
-        }
-        
-        connect[from].Add(to);
-        connect[to].Add(from);
-    }
-    
-    public int GetLine()
-    {
-        int c = 0;
-        foreach(HashSet<Point> h in connect.Values){
-            c += h.Count;
-        }
-        return c/2;
-    }
-    
-    public int solution(string dirs) {       
-        Point p;
-        p.x=0;
-        p.y=0;
-        for(int i=0;i<dirs.Length;i++){
-            char dir = dirs[i];
-            Point t;
-            t.x = p.x;
-            t.y = p.y;
-            
-            if(dir == 'U') t.y++;
-            if(dir == 'D') t.y--;
-            if(dir == 'L') t.x--;
-            if(dir == 'R') t.x++;
-            
-            if(!(-5 <= t.x && t.x <= 5) ||
-               !(-5 <= t.y && t.y <= 5)) continue;
-                        
-            if(t.x != p.x) Add(p, t);
-            else Add(p, t);
-            p = t;
-        }
-        
-        return GetLine();
-    }
+public class Solution
+{
+	public struct Point
+	{
+		public int X;
+		public int Y;
+	}
+
+	Dictionary<Point, HashSet<Point>> connect = new Dictionary<Point, HashSet<Point>>();
+
+	public void Add(Point from, Point to)
+	{
+		if (!connect.ContainsKey(from))
+		{
+			HashSet<Point> s = new HashSet<Point>();
+			connect.Add(from, s);
+		}
+
+		if (!connect.ContainsKey(to))
+		{
+			HashSet<Point> s = new HashSet<Point>();
+			connect.Add(to, s);
+		}
+
+		connect[from].Add(to);
+		connect[to].Add(from);
+	}
+
+	public int GetLine()
+		=> connect.Values.Sum(h => h.Count) / 2;
+
+	public int solution(string dirs)
+	{
+		Point p;
+		p.X = 0;
+		p.Y = 0;
+		foreach (var dir in dirs)
+		{
+			Point t;
+			t.X = p.X;
+			t.Y = p.Y;
+
+			if (dir == 'U') t.Y++;
+			if (dir == 'D') t.Y--;
+			if (dir == 'L') t.X--;
+			if (dir == 'R') t.X++;
+
+			if (!(-5 <= t.X && t.X <= 5) ||
+			    !(-5 <= t.Y && t.Y <= 5)) continue;
+
+			Add(p, t);
+			p = t;
+		}
+
+		return GetLine();
+	}
 }
